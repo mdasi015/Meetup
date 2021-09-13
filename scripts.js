91d89030132bfe313b23821f108f07120b9ac2a4
Mariana <marianads18@hotmail.com>
2021-09-13 12:21:29 -0300

    Meetup

---
+++ scripts.js
@@ -0,0 +1,54 @@
+
+/*console.log({
+    name: "Mariana",
+    idade: 23,
+    andar: function(){
+        console.log('andar')
+    }
+})
+console.log(["Mariana", 'Leite', 'Ovos', 3, 8, 9])
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+console.log(`${person.name} tem ${person.age} anos`)
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+
+console.log(animals[1])
+
+let weight
+
+console.log(typeof weight)*/
+
+let student = {
+    name: 'Mariana',
+    age: 23,
+    stars: 93.5,
+    isSubscribed: true,
+}
+//console.log(`${student.name} de idade ${student.age} pesa ${student.stars} kg`)
+
+let students = []
+
+students = [
+    student 
+]
+console.log(students[0])
+
+const John = {
+    name: "Maria",
+    age: 54,
+    weight: 65.4,
+    isSubscribed: true,
+}
+
+students[1] = john 
+console.log(students)
